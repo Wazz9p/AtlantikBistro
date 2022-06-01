f56c46539418eab47f14a8e6b3c649a62fcf0415
Wazz9p <git13ebruh@gmail.com>
2022-06-02 00:15:27 +0300

    Create category use case

---
+++ domain/src/main/java/com/wazz9p/domain/usecase/menu/FetchCategory.kt
@@ -0,0 +1,15 @@
+package com.wazz9p.domain.usecase.menu
+
+import com.wazz9p.domain.model.menu.Category
+import com.wazz9p.core.base.Result
+import com.wazz9p.domain.repository.menu.CategoryRepository
+import java.io.IOException
+
+class FetchMenu(private val categoryRepository: CategoryRepository) {
+
+    suspend fun execute(): Result<List<Category>> = try {
+        Result.Success(data = categoryRepository.getCategories())
+    } catch (e: IOException) {
+        Result.Error(e)
+    }
+}
