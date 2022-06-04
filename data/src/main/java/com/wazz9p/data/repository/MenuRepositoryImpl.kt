a9a7043a7e3b43ab9585bd7e71677adb3ec1893e
Wazz9p <git13ebruh@gmail.com>
2022-06-04 22:18:58 +0300

    Add menu repository implementation

---
+++ data/src/main/java/com/wazz9p/data/repository/MenuRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.wazz9p.data.repository
+
+import com.wazz9p.data.database.menu.localDataSourceImpl.MenuRoomDataSource
+import com.wazz9p.data.network.menu.MenuRemoteDataSource
+import com.wazz9p.domain.model.menu.Dish
+import com.wazz9p.domain.repository.menu.MenuRepository
+import java.net.UnknownHostException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MenuRepositoryImpl @Inject constructor(
+    private val localDataSource: MenuRoomDataSource,
+    private val remoteDataSource: MenuRemoteDataSource
+) : MenuRepository {
+
+    override suspend fun getMenu(categoryId: Int): List<Dish> {
+        return try {
+            val response = remoteDataSource.getMenu(categoryId)
+            localDataSource.saveMenu(response)
+            response
+        } catch (e: UnknownHostException) {
+            localDataSource.getMenu(categoryId)
+        }
+    }
+
+    override suspend fun getDish(dishId: Int): Dish = localDataSource.getDish(dishId)
+}
