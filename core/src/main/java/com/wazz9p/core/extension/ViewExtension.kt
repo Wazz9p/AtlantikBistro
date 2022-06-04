51bf24681ac18b4fce838d3f57620e69602559f6
Wazz9p <git13ebruh@gmail.com>
2022-06-04 20:04:46 +0300

    Add debounce click listener extension

--- core/src/main/java/com/wazz9p/core/extension/ViewExtension.kt
+++ core/src/main/java/com/wazz9p/core/extension/ViewExtension.kt
@@ -1,5 +1,6 @@
 package com.wazz9p.core.extension
 
+import android.os.SystemClock
 import android.view.View
 import android.view.View.*
 import android.view.ViewGroup
@@ -7,6 +8,40 @@ import androidx.core.view.forEach
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 
+fun View.setOnDebouncedClickListener(action: () -> Unit) {
+
+    val actionDebouncer = ActionDebouncer(action)
+
+    setOnClickListener {
+        actionDebouncer.notifyAction()
+    }
+}
+
+private class ActionDebouncer(private val action: () -> Unit) {
+
+    companion object {
+        const val DEBOUNCE_INTERVAL_MILLISECONDS = 600L
+    }
+
+    private var lastActionTime = 0L
+
+    fun notifyAction() {
+        val now = SystemClock.elapsedRealtime()
+
+        val millisecondsPassed = now - lastActionTime
+        val actionAllowed = millisecondsPassed > DEBOUNCE_INTERVAL_MILLISECONDS
+        lastActionTime = now
+
+        if (actionAllowed) {
+            action.invoke()
+        }
+    }
+}
+
+fun View.removeOnDebouncedClickListener() {
+    setOnClickListener(null)
+    isClickable = false
+}
 
 var View.visible
     get() = visibility == VISIBLE
