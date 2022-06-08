e1571ce02e461be68330e2bd38d848ffc158c5f1
Wazz9p <git13ebruh@gmail.com>
2022-06-08 16:46:05 +0300

    Fix class name

--- domain/src/main/java/com/wazz9p/domain/repository/news/NewsRepository.kt
+++ domain/src/main/java/com/wazz9p/domain/repository/news/NewsRepository.kt
@@ -2,7 +2,7 @@ package com.wazz9p.domain.repository.news
 
 import com.wazz9p.domain.model.news.News
 
-interface NewsRepositroy {
+interface NewsRepository {
     suspend fun getNewsList(): List<News>
-    suspend fun getNewsDetail(): News
+    suspend fun getNewsDetail(newsId: Int): News
 }
