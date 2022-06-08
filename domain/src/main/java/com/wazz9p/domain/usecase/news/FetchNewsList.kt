fc3290f280b5e134bb729e202d6a18b754750391
Wazz9p <git13ebruh@gmail.com>
2022-06-08 16:12:15 +0300

    Update repository variable name

--- domain/src/main/java/com/wazz9p/domain/usecase/news/FetchNewsList.kt
+++ domain/src/main/java/com/wazz9p/domain/usecase/news/FetchNewsList.kt
@@ -6,9 +6,9 @@ import com.wazz9p.domain.repository.news.NewsRepositroy
 import java.io.IOException
 import javax.inject.Inject
 
-class FetchNewsList @Inject constructor(private val newsRepositroy: NewsRepositroy) {
+class FetchNewsList @Inject constructor(private val newsRepository: NewsRepositroy) {
     suspend fun execute(): Result<List<News>> = try {
-        Result.Success(data = newsRepositroy.getNewsList())
+        Result.Success(data = newsRepository.getNewsList())
     } catch (e: IOException) {
         Result.Error(e)
     }
