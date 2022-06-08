86b9e748a5ba26a809cb8f420918911e4678e579
Wazz9p <git13ebruh@gmail.com>
2022-06-08 16:46:38 +0300

    Update variable names

--- domain/src/main/java/com/wazz9p/domain/usecase/news/FetchNewsList.kt
+++ domain/src/main/java/com/wazz9p/domain/usecase/news/FetchNewsList.kt
@@ -2,11 +2,11 @@ package com.wazz9p.domain.usecase.news
 
 import com.wazz9p.domain.model.news.News
 import com.wazz9p.core.base.Result
-import com.wazz9p.domain.repository.news.NewsRepositroy
+import com.wazz9p.domain.repository.news.NewsRepository
 import java.io.IOException
 import javax.inject.Inject
 
-class FetchNewsList @Inject constructor(private val newsRepository: NewsRepositroy) {
+class FetchNewsList @Inject constructor(private val newsRepository: NewsRepository) {
     suspend fun execute(): Result<List<News>> = try {
         Result.Success(data = newsRepository.getNewsList())
     } catch (e: IOException) {
