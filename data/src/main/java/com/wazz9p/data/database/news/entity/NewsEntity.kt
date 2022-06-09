1a3b36770c7a47eb009fb1365620630ae4384920
Wazz9p <git13ebruh@gmail.com>
2022-06-09 22:36:49 +0300

    Update news db entity primary key

--- data/src/main/java/com/wazz9p/data/database/news/entity/NewsEntity.kt
+++ data/src/main/java/com/wazz9p/data/database/news/entity/NewsEntity.kt
@@ -1,9 +1,11 @@
 package com.wazz9p.data.database.news.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "news")
 data class NewsEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val subtitle: String?,
