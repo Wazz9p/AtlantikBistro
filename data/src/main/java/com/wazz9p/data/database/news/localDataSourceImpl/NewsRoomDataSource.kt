f2a569acef404db79328a07de9364cdc8fdbb174
Wazz9p <git13ebruh@gmail.com>
2022-06-09 22:43:17 +0300

    Add news local data source implementation

---
+++ data/src/main/java/com/wazz9p/data/database/news/localDataSourceImpl/NewsRoomDataSource.kt
@@ -0,0 +1,23 @@
+package com.wazz9p.data.database.news.localDataSourceImpl
+
+import com.wazz9p.data.database.news.NewsLocalDataSource
+import com.wazz9p.data.database.news.dao.NewsDao
+import com.wazz9p.data.mappers.news.NewsEntityMapper
+import com.wazz9p.domain.model.news.News
+import javax.inject.Inject
+
+class NewsRoomDataSource @Inject constructor(
+    private val newsDao: NewsDao,
+    private val mapper: NewsEntityMapper
+) : NewsLocalDataSource {
+
+    override suspend fun saveNews(data: List<News>) {
+        data.map { mapper.mapToData(data = it) }
+            .let { newsDao.saveNews(data = it) }
+    }
+
+    override suspend fun getNewsList(): List<News> {
+        val data = newsDao.getNews()
+        return data.map { mapper.mapToDomain(data = it) }
+    }
+}
