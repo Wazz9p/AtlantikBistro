03a3a93a0ec74465e67c483e841cf5ef303a091e
Wazz9p <git13ebruh@gmail.com>
2022-06-10 12:58:40 +0300

    Update entity in db

--- data/src/main/java/com/wazz9p/data/database/AppDatabase.kt
+++ data/src/main/java/com/wazz9p/data/database/AppDatabase.kt
@@ -6,17 +6,27 @@ import com.wazz9p.data.database.menu.dao.CategoryDao
 import com.wazz9p.data.database.menu.dao.MenuDao
 import com.wazz9p.data.database.menu.entities.CategoryEntity
 import com.wazz9p.data.database.menu.entities.MenuEntity
+import com.wazz9p.data.database.news.dao.NewsDao
+import com.wazz9p.data.database.news.entity.NewsEntity
+import com.wazz9p.data.database.review.dao.ReviewDao
+import com.wazz9p.data.database.review.entity.ReviewEntity
+import com.wazz9p.domain.model.review.Review
 
 @Database(
     entities = [
         CategoryEntity::class,
-        MenuEntity::class
+        MenuEntity::class,
+        NewsEntity::class,
+        ReviewEntity::class
     ],
-    version = 2, exportSchema = false
+    version = 3, exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-
     abstract fun categories(): CategoryDao
 
     abstract fun menu(): MenuDao
+
+    abstract fun news(): NewsDao
+
+    abstract fun reviews(): ReviewDao
 }
