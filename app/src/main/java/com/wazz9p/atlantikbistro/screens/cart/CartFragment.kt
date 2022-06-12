e44c02ca822f80d7a8386d5326a509315bac6b87
Wazz9p <git13ebruh@gmail.com>
2022-06-13 01:44:57 +0300

    Create cart fragment

---
+++ app/src/main/java/com/wazz9p/atlantikbistro/screens/cart/CartFragment.kt
@@ -0,0 +1,10 @@
+package com.wazz9p.atlantikbistro.screens.cart
+
+import androidx.fragment.app.Fragment
+import com.wazz9p.atlantikbistro.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CartFragment : Fragment(R.layout.fragment_cart) {
+
+}
