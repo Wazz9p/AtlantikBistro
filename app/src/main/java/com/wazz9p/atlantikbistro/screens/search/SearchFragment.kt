0fdb48e809661c9793c466d5333b22acc9bccb39
Wazz9p <git13ebruh@gmail.com>
2022-06-13 01:40:02 +0300

    Create search fragment

---
+++ app/src/main/java/com/wazz9p/atlantikbistro/screens/search/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.wazz9p.atlantikbistro.screens.search
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.wazz9p.atlantikbistro.R
+import com.wazz9p.atlantikbistro.databinding.FragmentSearchBinding
+import com.wazz9p.core.delegate.viewBinding
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+    private val binding: FragmentSearchBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
