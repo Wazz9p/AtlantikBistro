47971377b313253adfc51db3a0a83f9b9af89e28
Wazz9p <git13ebruh@gmail.com>
2022-06-13 01:37:40 +0300

    Create about fragment

---
+++ app/src/main/java/com/wazz9p/atlantikbistro/screens/about/AboutFragment.kt
@@ -0,0 +1,10 @@
+package com.wazz9p.atlantikbistro.screens.about
+
+import androidx.fragment.app.Fragment
+import com.wazz9p.atlantikbistro.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AboutFragment : Fragment(R.layout.fragment_about) {
+
+}
